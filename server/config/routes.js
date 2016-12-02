1f03d45aab0bf29ce713f758708ee1426b810cda
jkspam2004 <jkspam2004@yahoo.com>
2016-12-02 00:35:26 -0800

    alternative way to export module

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,21 @@
 /* routes file */
-var plalia = require('../controllers/plalias.js');
+
+var fs = require('fs'); //included in express or node - reading file system
+var path = require('path'); //included in express or node - reading file system
+var controllers_path = path.join(__dirname, './../controllers');
+//Load all controllers
+var exports = {};
+fs.readdirSync(controllers_path).forEach(function(file) {
+    if (file.search(/.js$/) >= 0) {
+        var name = file.replace('.js', '');
+        exports[name] = require(controllers_path + '/' + file);
+    }
+});
+console.log(exports);
 
 module.exports = function(app) {
-    app.post('/hello', plalia.hello);
-    app.get('/', plalia.root);
-    app.get('/test', plalia.test);
-    app.get('*', plalia.unknown);
+    app.post('/hello', exports.plalias.hello);
+    app.get('/', exports.plalias2.root);
+    app.get('/test', exports.plalias.test);
+    app.get('*', exports.plalias.unknown);
 }
